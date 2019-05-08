abbdbfaa330597b752b4b687c2ded74c49028d9b
Nopelands <rhas@cin.ufpe.br>
2019-05-08 00:25:38 -0300

    why

--- tic-tac-toe/src/game.java
+++ tic-tac-toe/src/game.java
@@ -34,10 +34,39 @@ public class game {
                                 hard = true;
                             }
                         }
-                        int jogadas = 0;
-                        //TODO game here
+                        if (!hard) {
+                            boolean stop = false;
+                            while (!stop) {
+                                cpu_play_easy(board);
+                                board.print_board_state();
+                                if (board.win_check("X")) {
+                                    stop = true;
+                                    System.out.println("CPU wins");
+                                }
+                                if (board.count_empty_spots().length == 0) {
+                                    System.out.println("draw");
+                                    break;
+                                }
+                                boolean valid_move;
+                                String player_move;
+                                player_move = in.nextLine();
+                                valid_move = board.is_legal_move(convert_notation(player_move));
+                                while (!valid_move) {
+                                    player_move = in.nextLine();
+                                    valid_move = board.is_legal_move(convert_notation(player_move));
+                                    System.out.println("illegal move");
+                                }
+                                board.place_on_board("O", convert_notation(player_move));
+                                board.print_board_state();
+                                if (board.win_check("O")) {
+                                    stop = true;
+                                    System.out.println("Player wins");
+                                }
+                            }
+                        }
+                        //TODO hard here
                         break;
-                    case "step up":
+                    /*case "step up":
                         comando_menu_valido = true;
                         System.out.print("New Game wa Now Loading");
                         for (int i = 0; i < 3; i++) {
@@ -51,64 +80,42 @@ public class game {
                         break;
                     default:
                         System.out.println("unknown command");
-                        break;
+                        break;*/
                 }
             }
         }
     }
-    private static void cpu_play_easy(String[][] board) {
+    private static void cpu_play_easy(Board board) {
         int move = (int) (Math.random() * 9) + 1;
-        while (!is_legal_move(move, board)) {
+        while (!board.is_legal_move(move)) {
             move = (int) (Math.random() * 9) + 1;
         }
-        place_on_board("X", move, board);
-    }
-
-    private static void print_board_state(String[][] board) {
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                if (i < 3) {
-                    if (j < 3) {
-                        System.out.print(board[i][j] + "|");
-                    } else {
-                        System.out.print(board[i][j] + "|" + "\n");
-                        System.out.println("────────");
-                    }
-                } else {
-                    if (j < 3) {
-                        System.out.print(board[i][j] + "|");
-                    } else {
-                        System.out.print(board[i][j] + "|" + "\n\n");
-                    }
-                }
-            }
-        }
+        board.place_on_board("X", move);
     }
     private static int convert_notation(String position) {
         switch (position) {
             case "A1":
-                return 1;
-            case "A2":
-                return 2;
-            case "A3":
-                return 3;
-            case "B1":
-                return 4;
-            case "B2":
-                return 5;
-            case "B3":
-                return 6;
-            case "C1":
-                return 7;
-            case "C2":
-                return 8;
-            case "C3":
-                return 9;
-            default:
                 return 0;
+            case "A2":
+                return 1;
+            case "A3":
+                return 2;
+            case "B1":
+                return 3;
+            case "B2":
+                return 4;
+            case "B3":
+                return 5;
+            case "C1":
+                return 6;
+            case "C2":
+                return 7;
+            case "C3":
+                return 8;
+            default:
+                return 999;
         }
     }
-
     private static void sleep(int ms) {
         try {
             Thread.sleep(ms);
@@ -116,31 +123,60 @@ public class game {
             Thread.currentThread().interrupt();
         }
     }
+    private static boolean is_integer(String input) {
+        try {
+            Integer.parseInt(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 
     public static class Board {
-        private String[] board_state = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        private String[] board_state = {"0", "1", "2", "3", "4", "5", "6", "7", "8"};
 
         public String[] get_board_state() {
             return board_state;
         }
 
+        public int[] count_empty_spots() {
+            int empty_count = 0;
+            for (int i = 0; i < 9; i++) {
+                if (is_integer(board_state[i])) {
+                    empty_count++;
+                }
+            }
+            if (empty_count > 0) {
+                int[] answer = new int[empty_count];
+                for (int j = 0; j < empty_count; j++) {
+                    for (int i = 0; i < 9; i++) {
+                        if (is_integer(board_state[i])) {
+                            answer[j] = i;
+                            break;
+                        }
+                    }
+                }
+                return answer;
+            } else {
+                return new int[0];
+            }
+        }
+
         public boolean is_legal_move(int location) {
             switch (location) {
-                case 1: case 2: case 3: case 4: case 5: case 6: case 7: case 8: case 9:
-                    if (!board_state[location].equals("X") && !board_state[location].equals("O")) {
-                        return true;
-                    } else {
-                        return false;
-                    }
+                case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7: case 8:
+                    return !board_state[location].equals("X") && !board_state[location].equals("O");
                 default:
                     return false;
             }
         }
+
         public String get_field(int location) {
             return board_state[location];
         }
-        public void place_on_board(String player_type, int location, String[] board) {
-            board[location] = player_type;
+
+        public void place_on_board(String player_type, int location) {
+            board_state[location] = player_type;
         }
 
         public boolean win_check(String player) {
@@ -152,6 +188,40 @@ public class game {
                     (board_state[2].equals(player) && board_state[5].equals(player) && board_state[8].equals(player)) ||
                     (board_state[0].equals(player) && board_state[4].equals(player) && board_state[8].equals(player)) ||
                     (board_state[2].equals(player) && board_state[4].equals(player) && board_state[6].equals(player));
+        }
+
+        public void print_board_state() {
+            System.out.println("\\|1|2|3|");
+            System.out.println("────────");
+            System.out.print("A|");
+            for (int i = 0; i < 3; i++) {
+                if (is_integer(board_state[i])) {
+                    System.out.print(" |");
+                } else {
+                    System.out.print(board_state[i] + "|");
+                }
+            }
+            System.out.print("\n");
+            System.out.println("────────");
+            System.out.print("B|");
+            for (int i = 3; i < 6; i++) {
+                if (is_integer(board_state[i])) {
+                    System.out.print(" |");
+                } else {
+                    System.out.print(board_state[i] + "|");
+                }
+            }
+            System.out.print("\n");
+            System.out.println("────────");
+            System.out.print("C|");
+            for (int i = 6; i < 9; i++) {
+                if (is_integer(board_state[i])) {
+                    System.out.print(" |");
+                } else {
+                    System.out.print(board_state[i] + "|");
+                }
+            }
+            System.out.print("\n\n");
         }
     }
 }
